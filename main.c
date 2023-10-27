d1fc63d113fb17b5cb9d94b5736057b0eb5f50a5
Bibhabendu Mukherjee <73462296+BibhabenduMukherjee@users.noreply.github.com>
2023-10-27 20:46:45 +0530

    Update main.c implement join_path and recursive traversive function

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
+#define _DEFAULT_SOURCE
 #include <stdio.h>
+#include <assert.h>
 #include <dirent.h>
 #include <stdlib.h>  
-int main(int args , char **argss){
-    const char *dir_path = ".";
-    DIR *dir = opendir(dir_path);
+#define PATH_SEP "/"
+#define PATH_SEP_LEN (sizeof(PATH_SEP)-1)
+char *join_path(const char*base , const char *file){
+        size_t base_len = strlen(base);
+        size_t file_len = strlen(file);
+        char *result = malloc(base_len+file_len+ PATH_SEP_LEN +1);
+        assert(result != NULL);
+        char *end = result;
+       
+        memcpy(end, base , base_len);
+        end+= base_len;
+        memcpy(end, PATH_SEP , PATH_SEP_LEN);
+        end += PATH_SEP_LEN
+        memcpy(end, file , file_len);
+        end += file_len;
+        *end = '\0';
+        return result;
+}
+void print_file_rec(const char *dir_path){
+   DIR *dir = opendir(dir_path);
     //check if a directory exists
     if(dir == NULL){
         fprintf(stderr,"Could not open directory");
@@ -16,8 +35,24 @@ int main(int args , char **argss){
     struct dirent *ent = readdir(dir);
     while(ent != NULL){
         // maximum 256 it can able to work
-        printf("%s: ", ent->d_name);
+        if(ent->d_tyep == DT_DIR){
+             char*child_path = join_path(dir_path  , ent->d_name)
+             print_file_rec(child_path);
+             printf("%s: ", ent->d_name);
+             free(child_path);
+        }else{
+             printf("%s: ", ent->d_name);
+        }
+       
         ent = readdir(dir);
     }
+    closedir(dir);
+}
+
+
+
+int main(int args , char **argss){
+    const char *dir_path = ".";
+ 
     return 0;
 }
